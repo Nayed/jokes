858e8c965e55354d32141690861c217b9dee31de
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-11-30 00:34:35 +0100

    navbar & logout

--- resources/angular-material/app/app.js
+++ resources/angular-material/app/app.js
@@ -9,14 +9,24 @@ angular.module('myApp', [
     'satellizer'
 ])
 
- /*.config(function($mdThemingProvider) {
-   $mdThemingProvider.theme('default')
-    .primaryPalette('pink')
-    .accentPalette('orange');
- });
-*/
+.config(/*function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+    .primaryPalette('teal')
+    .accentPalette('lime')
+    },*/
+    ['$stateProvider', '$urlRouterProvider', '$authProvider', function($stateProvider, $urlRouterProvider, $authProvider) {
+        $authProvider.loginUrl = 'http://localhost:8000/api/v1/authenticate'
+        $urlRouterProvider.otherwise('/auth')
+    }]
+)
 
-.config(['$stateProvider', '$urlRouterProvider', '$authProvider', function($stateProvider, $urlRouterProvider, $authProvider) {
-    $authProvider.loginUrl = 'http://localhost:8000/api/v1/authenticate'
-    $urlRouterProvider.otherwise('/auth')
-}])
+.run(function($rootScope, $state, $auth) {
+    $rootScope.logout = () => {
+        $auth.logout().then(() => {
+            localStorage.removeItem('user')
+            $rootScope.currentUser = null
+            $state.go('auth')
+        })
+    }
+    $rootScope.currentUser = JSON.parse(localStorage.getItem('user'))
+})
